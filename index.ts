1aaa4795c6dbdef8659638fefe72de6cd641235a
Devon Crebbin <devon@land.org.au>
2025-09-12 01:42:57 +0800

    updated prompt

--- index.ts
+++ index.ts
@@ -1,3 +1,6 @@
+import { openai } from "@ai-sdk/openai";
+import { generateText } from "ai";
+
 async function retrieveDescriptionFromVideo(videoId: string) {
   const youtubePlayer: any = await retrieveYouTubePlayer(videoId);
   return youtubePlayer?.videoDetails?.shortDescription ?? null;
@@ -35,10 +38,26 @@ async function retrieveYouTubePlayer(videoId: string) {
 }
 
 async function main() {
-  console.log("Starting main");
-  const videoId = "Lc1McFzS-CM";
-  const description = await retrieveDescriptionFromVideo(videoId);
-  console.log(description);
+  const description = await retrieveDescriptionFromVideo("Lc1McFzS-CM");
+  const contributors = await ai(description);
+  console.log(contributors);
+}
+
+const prompt = `Using the following text, extract the song and or music video contributors with their role and an array of the contributors in JSON format.
+<example>
+{
+  "example role 1": ["example person 1", "example person 2", "example person 3"],
+  "example role 2": ["example person 1"],
+}
+</example>`;
+
+async function ai(input: string) {
+  const { text } = await generateText({
+    model: openai("gpt-4.1-mini"),
+    prompt: `${prompt} <text>${input}</text>`,
+  });
+  const jsonData = text.replace(/```json/g, "").replace(/```/g, "");
+  return JSON.parse(jsonData);
 }
 
 main();
